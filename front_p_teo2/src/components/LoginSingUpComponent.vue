ccad7c516ded4e432ede99ed7ec1d97ee34a4bc0
cresb-49 <carlosbpac@gmail.com>
2024-03-02 23:30:29 -0600

    Update container margin-top in LoginSingUpComponent.vue

--- front_p_teo2/src/components/LoginSingUpComponent.vue
+++ front_p_teo2/src/components/LoginSingUpComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container mt-3">
         <div class="sombra">
             <div class="tabs-container">
                 <div class="tabs-item tab-login" @click="HandleTabClick(tabNames.LOGIN)"
