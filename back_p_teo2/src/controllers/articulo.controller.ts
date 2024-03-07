2c20303549634bea076408631b897204dc34506b
cresb-49 <carlosbpac@gmail.com>
2024-03-07 05:41:13 -0600

    Update article controller and add CreatePublicacionComponent

--- back_p_teo2/src/controllers/articulo.controller.ts
+++ back_p_teo2/src/controllers/articulo.controller.ts
@@ -51,5 +51,41 @@ export const createArticulo = async (req: Request, res: Response) => {
 };
 
 export const updateArticulo = async (req: Request, res: Response) => {
-
+    const { nombre, precio, cantidad, id_categoria, descripcion, imagen } = req.body;
+    const { idArticulo } = req.params;
+    const { tokenPayload } = req;
+    const idUsuario = tokenPayload.usuarioId;
+    let payload = {
+        nombre: nombre,
+        valor: precio,
+        descripcion: descripcion,
+        id_categoria: id_categoria,
+        id_usuario: idUsuario,
+        cantidad: cantidad
+    }
+    //Validamos el precio y la cantidad positiva
+    if (precio <= 0) {
+        return responseAPI(HttpStatus.BAD_REQUEST, res, null, "El precio no puede ser negativo o cero", "El precio no puede ser negativo o cero");
+    }
+    if (cantidad <= 0) {
+        return responseAPI(HttpStatus.BAD_REQUEST, res, null, "La cantidad no puede ser negativa o cero", "La cantidad no puede ser negativa o cero");
+    }
+    //Buscamos el articulo si existe mediante el id y el id del usuario
+    Articulo.findOne({ where: { id: idArticulo, id_usuario: idUsuario } })
+        .then((articulo: any) => {
+            if (articulo) {
+                Articulo.update(payload, { where: { id: idArticulo } })
+                    .then((value: any) => {
+                        return responseAPI(HttpStatus.OK, res, value, "Producto Actualizado");
+                    })
+                    .catch((reason: any) => {
+                        return responseAPI(HttpStatus.INTERNAL_SERVER_ERROR, res, null, reason.message, reason.message);
+                    })
+            } else {
+                return responseAPI(HttpStatus.NOT_FOUND, res, null, "Producto no encontrado", "Producto no encontrado");
+            }
+        })
+        .catch((reason: any) => {
+            return responseAPI(HttpStatus.INTERNAL_SERVER_ERROR, res, null, reason.message, reason.message);
+        })
 };
