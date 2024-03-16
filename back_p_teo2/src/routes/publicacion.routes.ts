56f3599e907ea68b26872455e91174e3812eec42
cresb-49 <carlosbpac@gmail.com>
2024-03-15 20:33:13 -0600

    Add routes for getting unconfirmed publications

--- back_p_teo2/src/routes/publicacion.routes.ts
+++ back_p_teo2/src/routes/publicacion.routes.ts
@@ -1,16 +1,19 @@
 import { verifyToken } from "../middleware/authMiddleware";
+import { isConfirm } from "../middleware/roles.middelware";
 import { Router } from 'express';
-import { getPublicaciones, getPublicacionesUsuario, createPublicacion, updatePublicacion, getTipoPublicacion, getPublicacion } from "../controllers/publicacion.controller";
+import { getPublicaciones, getPublicacionesUsuario, createPublicacion, updatePublicacion, getTipoPublicacion, getPublicacion, getPublicacionesPorConfirmar, confirmarPublicacion } from "../controllers/publicacion.controller";
 
 const router = Router()
 
 router.get('/publicaciones', getPublicaciones);
+router.get('/publicaciones/sin-confirmar', getPublicacionesPorConfirmar);
 router.get('/publicaciones/tipo', getTipoPublicacion);
 router.get('/publicaciones/IdUsuario', getPublicacionesUsuario)
 
 router.get('/publicacion/:id', getPublicacion);
 
-router.post('/publicacion',verifyToken, createPublicacion)
+router.post('/publicacion', verifyToken, createPublicacion)
+router.post('/publicacion/confirmar', verifyToken, isConfirm, confirmarPublicacion)
 router.put('/publicacion', updatePublicacion)
 
 
