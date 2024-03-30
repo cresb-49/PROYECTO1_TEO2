031dbba430704c6e31ae476bfbd6a66248e98057
cresb-49 <carlosbpac@gmail.com>
2024-03-30 14:59:28 -0600

    Update API base URL in main.js

--- front_p_teo2/src/main.js
+++ front_p_teo2/src/main.js
@@ -11,11 +11,12 @@ import router from './router'
 
 import storage from './storage';
 
-axios.defaults.baseURL='http://localhost:3000/api';
+// axios.defaults.baseURL='http://localhost:3000/api'; // Conexion al api sin el reverse proxy
+axios.defaults.baseURL = 'http://localhost/api'; // Conexion al api con el reverse proxy
 
 // let token = localStorage.getItem('token');
 // axios.defaults.headers.common['Authorization']='Bearer '+token;
 
-createApp(App).use(router).use(storage).use(VueAxios,axios).mount('#app')
+createApp(App).use(router).use(storage).use(VueAxios, axios).mount('#app')
 
 import 'bootstrap/dist/js/bootstrap.js'
